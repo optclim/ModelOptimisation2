dc98042e655ad5dfc07dafe0d7229fa134912a77
mhagdorn <magnus.hagdorn@marsupium.org>
2022-04-01 12:09:57 +0100

    add dummy model

--- ModelOptimisation2/model_config.py
+++ ModelOptimisation2/model_config.py
@@ -3,11 +3,13 @@ import logging
 import json
 from pathlib import Path
 
+from .dummy import DummyModel
 from .UKESM import UKESM
 from .MITgcm import MITgcm
 from .HadCM3 import HadCM3
 
-MODELS = {'UKESM': UKESM,
+MODELS = {'DummyModel': DummyModel,
+          'UKESM': UKESM,
           'MITgcm': MITgcm,
           'HadCM3': HadCM3}
 
