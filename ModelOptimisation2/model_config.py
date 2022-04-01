8d51f81221a69ec5e5832e67ce351c619d0916db
mhagdorn <magnus.hagdorn@marsupium.org>
2022-04-01 16:06:52 +0100

    get parameter set from ObjectiveFunction DB

--- ModelOptimisation2/model_config.py
+++ ModelOptimisation2/model_config.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import json
 from pathlib import Path
+import shutil
+import ObjectiveFunction
 
 from .dummy import DummyModel
 from .UKESM import UKESM
@@ -18,20 +20,55 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('parameters', type=Path,
-                        help="name of file containing parameters to apply")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-p', '--parameters', type=Path,
+                       help="name of file containing parameters to apply")
+    group.add_argument('-c', '--config', type=Path,
+                       help="name of configuration file")
     parser.add_argument('modeldir', type=Path,
                         help="name of model directory")
     parser.add_argument('-t', '--model-type', choices=MODELS.keys(),
                         default='UKESM',
                         help="select the model type")
+    parser.add_argument('-d', '--default-values', action='store_true',
+                        default=False,
+                        help="use default values from config file")
+    parser.add_argument('-C', '--clone', help="model setup to clone")
     args = parser.parse_args()
 
-    params = json.loads(args.parameters.read_text())
+    modeldir = args.modeldir
+    if args.parameters is not None:
+        params = json.loads(args.parameters.read_text())
+        model = args.model_type
+    else:
+        config = ObjectiveFunction.ObjFunConfig(args.config)
+        model = config.cfg['setup']['model']
+        if args.default_values:
+            params = config.values
+            cdir = Path('default')
+            runid = None
+        else:
+            try:
+                runid, params = config.objectiveFunction.get_new(with_id=True)
+            except ObjectiveFunction.NoNewRun as e:
+                parser.error(e)
+            cdir = Path(f'run_{runid:04d}')
 
-    model = MODELS[args.model_type](args.modeldir)
+        if args.clone:
+            modeldir = modeldir / cdir
+            clonedir = Path(args.clone)
+            if not clonedir.is_dir():
+                parser.error(f'clone directory {clonedir} does not exist')
+            if modeldir.exists():
+                parser.error(f'target directory {modeldir} already exists')
+            shutil.copytree(args.clone, modeldir)
+            if runid is not None:
+                (modeldir / 'objfun.runid').write_text(f'{runid}')
+
+    model = MODELS[model](modeldir)
     model.write_params(params)
 
+    print(modeldir)
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'f90nml',
+        'ObjectiveFunction',
     ],
     extras_require={
         'lint': [
