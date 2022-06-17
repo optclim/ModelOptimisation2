79be93ed11fdd06bb1a75528b91b3888edb42c0d
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-06-17 12:20:26 +0100

    add option to run optimiser until all new parameter sets are requested

--- ModelOptimisation2/optimise.py
+++ ModelOptimisation2/optimise.py
@@ -15,16 +15,7 @@ def residual(cfg, x):
     return residual
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('config', type=Path,
-                        help="name of configuration file")
-    args = parser.parse_args()
-
-    config = ModelOptimisationConfig(args.config)
-
+def run_opt(config):
     for i in range(2):
         # start with lower bounds
         try:
@@ -41,13 +32,45 @@ def main():
         except ObjectiveFunction_client.PreliminaryRun:
             logging.info('new parameter set')
             continue
+        return x
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=Path,
+                        help="name of configuration file")
+    parser.add_argument('-g', '--generate-all', action='store_true',
+                        default=False, help="generate all tasks")
+    args = parser.parse_args()
+
+    config = ModelOptimisationConfig(args.config)
+
+    if args.generate_all:
+        num_tasks = 0
+        waiting = False
+        while True:
+            try:
+                x = run_opt(config)
+                break
+            except ObjectiveFunction_client.NewRun:
+                num_tasks += 1
+            except ObjectiveFunction_client.Waiting:
+                waiting = True
+                break
+        if waiting:
+            print(num_tasks)
+            sys.exit(3)
+    else:
+        try:
+            x = run_opt(config)
         except ObjectiveFunction_client.NewRun:
             print('new')
             sys.exit(1)
         except ObjectiveFunction_client.Waiting:
             print('waiting')
             sys.exit(2)
-        break
 
     logging.info(f"optimum at {x}")
     print('done')
